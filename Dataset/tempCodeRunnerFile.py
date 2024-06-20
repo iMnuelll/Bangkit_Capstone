6cc527ae9f10382366b1f1c07472aa96097e6389
Leonard091 <leonardosirait80@gmail.com>
2024-06-20 16:25:50 +0700

    cleaning file

--- Dataset/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-# Print sample data
-# print("Sample User Data:")
-# print(user_data[:3])  # Print 3 sample data pengguna
-# print("\nSample Task Data:")
-# print(task_data[:3])  # Print 3 sample data tugas
